6911042a605b0077507e45fc26b1aed604ef5de4
Sam Shrum <Sam.L.Shrum@gmail.com>
2021-03-26 18:39:53 -0400

    Matches from JSON

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -12,9 +15,10 @@ import (
 )
 
 type Match struct {
-    Time string
+    Time time.Time
     Opponent string
     Venue string
+    Competition string
     StreamLink string
     StreamTitle string
 }
@@ -26,14 +30,33 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
+	files, err := ioutil.ReadDir("./data")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	var matches []Match
+    for _, file := range files {
+        fmt.Println(file.Name())
+		dat, _ := ioutil.ReadFile("./data/" + file.Name())
+		fmt.Printf("%+v\n", dat)
+		var fileMatches []Match
+		if err := json.Unmarshal(dat, &fileMatches); err != nil {
+			panic(err)
+		}
+		matches = append(matches, fileMatches...)
+    }
+
+	sort.Slice(matches[:], func(i, j int) bool {
+		return matches[i].Time.Before(matches[j].Time)
+	})
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		var matches []Match
-		matches = append(matches, Match {Time: time.Now().Format("Mon Jan 2 15:04"), Opponent: "US Soccer", Venue: "The Galaxy", StreamLink: "https://youtube.com", StreamTitle: "Youtube"})
 		fmt.Printf("%+v\n", matches)
 		c.HTML(http.StatusOK, "index.tmpl.html", gin.H {
 			"Message": "",
